ab4e3f1cd371e18c965b8688d90f3e4ebb44f59b
ReptiloidAnunakTestgit config --global user.email stepanovartemijdev@gmail.comgit config user.name ReptiloidAnunakTestgit config user.email stepanovartemijdev@gmail.comgit config --global user.name ReptiloidAnunak <amanitocornholio@proton.me>
2025-08-11 22:02:17 -0300

    READY: all forms of notifications

--- providers/telegram_provider.py
+++ providers/telegram_provider.py
@@ -3,8 +3,6 @@ from telegram import Bot
 from telegram.error import TimedOut
 from settings import TG_BOT_TOKEN, TG_BOT_CHAT
 
-# providers/telegram_ptb.py (асинхронная отправка)
-
 async def _send(text: str):
     bot = Bot(token=TG_BOT_TOKEN)
     try:
